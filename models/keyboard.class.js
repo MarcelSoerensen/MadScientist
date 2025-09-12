040ed58fbc52cdf200762bc50a8e9d83c492d32f
Marcel Sörensen <marcel.soerensen@icloud.com>
2025-09-12 09:32:48 +0200

    Removed unnecessary type annotations for keyboard input state properties in the Keyboard class.

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -2,20 +2,12 @@
  * Handles keyboard input state for the game
  */
 class Keyboard {
-    /** @type {boolean} Left arrow key state */
     LEFT = false;
-    /** @type {boolean} Right arrow key state */
     RIGHT = false;
-    /** @type {boolean} Up arrow key state */
     UP = false;
-    /** @type {boolean} Down arrow key state */
     DOWN = false;
-    /** @type {boolean} Spacebar key state */
     SPACE = false;
-    /** @type {boolean} D key state */
     D = false;
-    /** @type {boolean} Y key state */
     Y = false;
-    /** @type {boolean} S key state */
     S = false;
 }
