d2092d7e9d1bdc9528c59a4c0d7f36642b81cb0e
Marcel Sörensen <marcel.soerensen@icloud.com>
2025-09-27 13:26:53 +0200

    Update background object positions in level 1 for improved layout

--- levels/level-1.js
+++ levels/level-1.js
@@ -19,13 +19,13 @@ function createLevel1() {
         ],
         [
             new BackgroundObject('img/Backgrounds/1.png', -400, 0, 480, 1954),
-            new BackgroundObject('img/Backgrounds/7.png', -400, 0, 80, 1955),
-            new BackgroundObject('img/Backgrounds/8.png', -402, 422, 58, 1957),
+            new BackgroundObject('img/Backgrounds/7.png', -500, 0, 80, 1955),
+            new BackgroundObject('img/Backgrounds/8.png', -600, 422, 58, 1957),
             new BackgroundObject('img/Backgrounds/2.png', 1552, 0, 480, 1954),
-            new BackgroundObject('img/Backgrounds/9.png', 1552, 0, 80, 1957),
-            new BackgroundObject('img/Backgrounds/10.png', 1552, 25, 455, 1955),
-            new BackgroundObject('img/Backgrounds/7.png', 1952*2-480, 0, 80, 1955),
-            new BackgroundObject('img/Backgrounds/8.png', 1952*2-478, 422, 58, 1957),
+            new BackgroundObject('img/Backgrounds/9.png', 1452, 0, 80, 1957),
+            new BackgroundObject('img/Backgrounds/10.png', 1352, 25, 455, 1955),
+            new BackgroundObject('img/Backgrounds/7.png', 1952*2-500, 0, 80, 1955),
+            new BackgroundObject('img/Backgrounds/8.png', 1952*2-600, 422, 58, 1957),
         ]
     );
 }
